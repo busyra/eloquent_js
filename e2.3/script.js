085905708122a79b07276a594955eb15b8291f37
Busyra Anwar <busyra@me.com>
2015-08-14 15:46:02 -0700

    fixes variables

--- e2.3/script.js
+++ e2.3/script.js
@@ -1,8 +1,8 @@
-//creates hash chess board 
+//creates hash chess board
 var size = 8;
 var chess = "";
-for (var a = 0; y < size; a++) {
-  for (var b = 0; x < size; b++) {
+for (var a = 0; a < size; a++) {
+  for (var b = 0; b < size; b++) {
     if ((a + b) % 2 == 0){
       board += " ";
     }
