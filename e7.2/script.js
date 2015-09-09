6f6543c96da01ac544464092f0ca205aec803d26
Busyra Anwar <busyra@me.com>
2015-09-08 22:57:38 -0700

    started Smart Palent Eater

---
+++ e7.2/script.js
@@ -0,0 +1,42 @@
+function SmartPlantEater() {
+      this.energy = 30;
+      this.direction = "e";
+}
+SmartPlantEater.prototype.act = function(view) {
+      var space = view.find(" ");
+      if (this.energy > 90 && space)
+            return {type: "reproduce", direction: space};
+      var plants = view.findAll("*");
+      if (plants.length > 1)
+            return {type: "eat", direction: randomElement(plants)};
+      if (view.look(this.direction) != " " && space)
+            this.direction = space;
+      return {type: "move", direction: this.direction};
+};
+
+//not done yet
+
+animateWorld(new LifelikeWorld(
+      ["####################################################",
+       "#                 ####         ****              ###",
+       "#   *  @  ##                 ########       OO    ##",
+       "#   *    ##        O O                 ****       *#",
+       "#       ##*                        ##########     *#",
+       "#      ##***  *         ****                     **#",
+       "#* **  #  *  ***      #########                  **#",
+       "#* **  #      *               #   *              **#",
+       "#     ##              #   O   #  ***          ######",
+       "#*            @       #       #   *        O  #    #",
+       "#*                    #  ######                 ** #",
+       "###          ****          ***                  ** #",
+       "#       O                        @         O       #",
+       "#   *     ##  ##  ##  ##               ###      *  #",
+       "#   **         #              *       #####  O     #",
+       "##  **  O   O  #  #    ***  ***        ###      ** #",
+       "###               #   *****                    ****#",
+       "####################################################"],
+      {"#": Wall,
+       "@": Tiger,
+       "O": SmartPlantEater, // from previous exercise
+       "*": Plant}
+));
