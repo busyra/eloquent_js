46b7f073a3555e815f627e90009b90f40389dcd1
Busyra Anwar <busyra@me.com>
2015-09-20 20:48:55 -0700

    content negotiation

--- e2.2/script.js
+++ e2.2/script.js
@@ -1,5 +1,5 @@
 //prints numbers 1...100 and replaces with fizz when divisible by 3 and buzz when divisible 5 and fizzbuzz when divisible by both
-for (var counter = 1; counter <= 100; counter++){
+for (var counter = 1; counter <= 1000; counter++){
   if (counter % 3 == 0 && counter % 5 == 0) {
     console.log("fizzbuzz");
   }else if(counter % 3 == 0){
